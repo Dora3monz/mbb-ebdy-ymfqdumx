f1102f684cba2c39a9714f489ea1ff079ab88557
Dora3monZ <haribinhamdan90@gmail.com>
2016-04-18 12:37:48 +0800

    t

--- platforms/android/build/intermediates/assets/debug/www/templates/map-and-location/ios-map-connect/js/controllers.js
+++ platforms/android/build/intermediates/assets/debug/www/templates/map-and-location/ios-map-connect/js/controllers.js
@@ -10,7 +10,7 @@ appControllers.controller('iosMapConnectCtrl', function ($scope) {
     // It will initial all variable data and let the function works when page load.
     $scope.initialForm = function () {
         //destinationLocation is latitude,longitude of the destination location.
-        $scope.destinationLocation = "3.070378,101.517234";
+        $scope.destinationLocation = "2.937297,101.705073";
     };// End initialForm
 
     // openIosMap is for open IOS Map application.
